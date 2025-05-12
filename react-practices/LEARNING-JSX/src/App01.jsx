44c7cb6848a47cbb0531f45b12403325dd4d2cd1
haribo-119 <kangwyz1@gmail.com>
2025-05-13 00:38:18 +0900

    conditional

--- react-practices/LEARNING-JSX/src/App01.jsx
+++ react-practices/LEARNING-JSX/src/App01.jsx
@@ -17,15 +17,25 @@ function getUserContent(userStatus){
     );
 }
 
-// 2) AND 연산자 활용
+// 2) 연산자 활용
 const hasMessage = true
 const message = "Hello, World!"
 const nullCheck = null
-const nullCheck1 = ""
+const emptyStr = ""
+
+// 3) 배열
+const fruits = ["Apple","Banana","Cherry"]
+const books = [
+    {id:1, title:'React Basics',published:true,publisher:'Manning'},
+    {id:2, title:'Advenced Hooks',published:false,publisher:'OReilly'},
+    {id:3, title:'JSX in Depth',published:true,publisher:'Packt'}
+];
+const publisheds = books.filter(book => book.published)
 
 function App01() {
   return(
-    <>
+    <>  1) 조건부
+
         {getUserContent("admin")}
         {hasMessage && <h2>You have new messages!</h2>}
         {/*hasMessage값이 참일 경우 뒤의 메세지가 출력  */}
@@ -33,22 +43,46 @@ function App01() {
         {/*message값이 비어 있지 않으므로 뒤에 메세지가 출력 */}
 
 
-        <p>
+        <p> 2) 연산자
+            <br/>
             ---Null Check연산자(??)---
             <br/>
             nullCheck : {nullCheck ?? <em>No message</em>}
-            <br/>
-            nullCheck1 : {nullCheck1 ?? <em>No message</em>}
             {/* nullCheck = null 인경우 위에 메세지 출력  */}
+            <br/>
+            emptyStr : {emptyStr ?? <em>No message</em>}
+            {/* emptyStr = "" 인경우 위에 메세지 출력X  */}
         </p>
         <p>
             ---Null Check연산자(||)---
             <br/>
             nullCheck : {nullCheck || <em>Empty</em>}  
-            <br/>
-            nullCheck1 : {nullCheck1 || <em>Empty</em>}  
             {/* nullCheck = null 인경우 위에 메세지 출력  */}
+            <br/>
+            emptyStr : {emptyStr || <em>Empty</em>}  
+            {/* emptyStr = "" 인경우 위에 메세지 출력 */}
         </p>
+            3) 배열
+            <br/>
+            {[0,123,'A','Hello',true,false]} {/*출력 값 : 0123AHello  */}
+            <br/>
+            <ul>
+                {fruits.map((fruits,index) => (
+                    <li key={index}>{fruits}</li>
+                ))}
+            </ul>
+            <br/>
+            {publisheds.length > 0 && <h2>Published Books</h2>}
+            {publisheds.length ?
+              publisheds.map(book =>
+                <article key={book.id}>
+                    <strong>{book.title}</strong>
+                    <em>-{book.publisher}</em>
+                </article>
+              )
+              :<p>No published books found.</p>
+            }
+
     </>
   )
 }
