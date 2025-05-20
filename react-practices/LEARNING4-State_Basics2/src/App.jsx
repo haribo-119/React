4550bd21ad4b7d3eeabfee6e0dd88f59e4bfae90
haribo-119 <kangwyz1@gmail.com>
2025-05-20 23:53:14 +0900

    State Basics

---
+++ react-practices/LEARNING4-State_Basics2/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import {useState} from 'react'
+import Profile from './Profile'
+
+function App(){
+  const users = ['Alice','Bob','Clark']
+  const [user, setUser] = useState(users[0])
+  const [status, setStatus] = useState(true)
+
+  console.log('App Rendered')
+
+  return(
+    <>
+      <h2>User Profile</h2>
+      <button onClick={()=> setStatus(!status)}>
+        Toggle Status
+      </button>
+      <button onClick={
+        () => setUser(
+          users[(users.indexOf(user)+1) % users.length]
+        )}>
+        Switch User
+      </button>
+      <p>
+        <b>{user}</b> | {status ? 'Active':'Inactive'}
+      </p>
+
+      <Profile name={user}/>
+    </>
+  )
+
+}
+
+export default App
