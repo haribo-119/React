b0391dfa29d39a7808762db94c01b9be9852214c
haribo-119 <kangwyz1@gmail.com>
2025-05-24 00:55:03 +0900

    useReducer

--- react-practices/LEARNING5-useReducer/src/App.jsx
+++ react-practices/LEARNING5-useReducer/src/App.jsx
@@ -7,10 +7,16 @@ import { useState } from 'react'
 // from './reducers/userReducer'
 
 // 2) data.js
-import React, { useReducer } from 'react' 
-import { userReducer, init }
-from './reducers/userReducer'
-import data from './data'
+// import React, { useReducer } from 'react' 
+// import { userReducer, init }
+// from './reducers/userReducer'
+// import data from './data'
+
+// 3) countReducer.js
+import React, { useReducer } from 'react'
+import { countReducer, initialState }
+ from './reducers/countReducer'
+
 
 function App() {
   // const [name, setName] = useState('')
@@ -66,38 +72,62 @@ function App() {
 //  const [state, dispatch] // 대부분 state, dispatch 라고 명시 (변수 이름은 변경가능)
 //   = useReducer(userReducer, initialState) // useReducer(함수, 초기값)
 
-    // 2) data.js
-    const [state,dispatch]
-    = useReducer (userReducer,data,init) 
+  //   // 2) data.js
+  //   const [state,dispatch]
+  //   // userReducer.js에 init은 init함수를 호출, data는 파라미터 값으로 들어감
+  //   = useReducer (userReducer,data,init)  
 
-  return (
-    <div>
-      <input
-        type="text" placeholder="Enter name"
-        value={state.name}
-        onChange={(e) => dispatch({ 
-          // userReducer.js 에서
-          // type은 userReducer(state, action)에서 action값으로 입력됨
-          // payload는 userReducer(state, action)에서  return문의 ation.payload로 입력됨
-          type: 'SET_NAME',  payload: e.target.value })}
-      />
-      <input
-        type="number" placeholder="Enter birth year"
-        value={state.year}
-        onChange={(e) => dispatch({ 
-          type: 'SET_YEAR', payload: e.target.value })}
-      />
-      {state.warning
-       && <p style={{ color: 'red' }}>{state.warning}</p>}
-      <p>Name: {state.name}</p>
-      <p>Year: {state.year}</p>
-      {/* 2) data.js */}
-      <button onClick={
-        () => dispatch({ type: 'RESET', payload: data })}>
-        Reset
-      </button>
+  // return (
+  //   <div>
+  //     <input
+  //       type="text" placeholder="Enter name"
+  //       value={state.name}
+  //       onChange={(e) => dispatch({ 
+  //         // userReducer.js 에서
+  //         // type은 userReducer(state, action)에서 action값으로 입력됨
+  //         // payload는 userReducer(state, action)에서  return문의 ation.payload로 입력됨
+  //         type: 'SET_NAME',  payload: e.target.value })}
+  //     />
+  //     <input
+  //       type="number" placeholder="Enter birth year"
+  //       value={state.year}
+  //       onChange={(e) => dispatch({ 
+  //         type: 'SET_YEAR', payload: e.target.value })}
+  //     />
+  //     {state.warning
+  //      && <p style={{ color: 'red' }}>{state.warning}</p>}
+  //     <p>Name: {state.name}</p>
+  //     <p>Year: {state.year}</p>
+  //     {/* 2) data.js */}
+  //     <button onClick={
+  //       () => dispatch({ type: 'RESET', payload: data })}>
+  //       Reset
+  //     </button>
 
-    </div>
+  //   </div>
+  // )
+
+
+  // 3) countReducer.js
+  const [state, dispatch]
+  = useReducer(countReducer, initialState)
+
+  const handleClick = (type, value, event) => {
+    // clientX, clientY는 브라우저가 자동으로 제공하는 이벤트 객체의 속성
+    const {clientX:x, clientY: y} = event
+    dispatch({
+      type, payload:{value}, meta:{x,y}
+    })
+  }
+
+  return(
+    <>
+      <p>Count : {state.count}</p>
+      <button onClick={(e) => handleClick('INC',1,e)}>+1</button>
+      <button onClick={(e) => handleClick('DEC',1,e)}>-1</button>
+      <button onClick={(e) => handleClick('INC',2,e)}>+2</button>
+      <button onClick={(e) => handleClick('DEC',2,e)}>-2</button>
+    </>
   )
 
 }
